8c96f5ec2a2dfb7756af6309cfcb3250222ec313
aliebadi65 <aliebadi66@gmail.com>
2016-06-15 16:21:59 +0430

    Update set_typing.lua

--- plugins/set_typing.lua
+++ plugins/set_typing.lua
@@ -3,7 +3,7 @@ local hash = 'bot:typing'
 if is_admin(msg) then
 if matches[1] == "typing" and matches[2] == 'yes' then
 redis:set(hash, "on")
-return "Typing > on"
+return "اااااااااااااا > on"
 elseif matches[1] == "typing" and matches[2] == 'no' then
 redis:del(hash)
 return "Typing > off"
